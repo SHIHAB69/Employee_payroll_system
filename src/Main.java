bde14670d880cec7206a47c692ad633a63de0e88
SHIHAB69 <southcybersoldier@gmail.com>
2024-08-16 14:42:20 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+abstract class Employee{
+
+    private String name;
+    private int id;
+
+    public Employee(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
